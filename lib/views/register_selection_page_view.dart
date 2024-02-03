14309e0e1a1835b4e6c1d962ddc5d895603def40
Kofi Agyemang <ebensakyi@gmail.com>
2024-02-03 23:33:56 +0000

    -

--- lib/views/register_selection_page_view.dart
+++ lib/views/register_selection_page_view.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../../../../widgets/small-button.dart';
-import '../../../routes/app_pages.dart';
 import '../controllers/auth_page_controller.dart';
 
 // MediaQuery.of(context).size.height -    kToolbarHeight -
